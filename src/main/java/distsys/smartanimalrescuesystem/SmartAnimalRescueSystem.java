8fdaa5d2e2e0560bd1a8d2b04036d4ca521f9100
mariRoliveira0 <rodrigues.mariana1995@gmail.com>
2026-03-27 09:46:41 +0000

    Initial Maven project setup

---
+++ src/main/java/distsys/smartanimalrescuesystem/SmartAnimalRescueSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.smartanimalrescuesystem;
+
+/**
+ *
+ * @author marianarodriguesoliveira
+ */
+public class SmartAnimalRescueSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
